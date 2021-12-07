784054846912816ec0fa1de2e90226f2a5f4a03a
artan09 <araja002@osloskolen.no>
2021-12-07 09:50:07 +0100

    stuff

--- art_login.php
+++ art_login.php
@@ -123,8 +123,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
             </div>
             <p>Don't have an account? <a href="art_reges.php">Sign up now</a>.</p>
+            <p>need to reset your password? <a href="reset-password.php">you can do it here</a>.</p>
         </form>
     </div>
 </body>
